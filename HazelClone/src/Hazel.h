a5aadb04ec4b87ceb36b66105b52190128335262
Trung <bomcon123456@gmail.com>
2018-11-05 11:10:03 +0700

    Setup basic Application and Entry Point.

---
+++ HazelClone/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Standard lib
+#include <stdio.h>
+
+// FOR USE BY HAZEL'S APPLICATION
+#include "Hazel/Application.h"
+
+// -- Entry point-----------
+#include "Hazel/EntryPoint.h"
+// -- Entry point-----------
